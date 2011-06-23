1b70dbabf2371de181cc09fb578365fe62712972
David Cramer <dcramer@gmail.com>
2011-06-23 19:08:41 +0200

    Fix cutoff logic

--- sentry/core/cleaner.py
+++ sentry/core/cleaner.py
@@ -21,14 +21,14 @@ class Cleaner(threading.Thread):
             # XXX: this could be more efficient if we took interest in what
             # groups an event is part of, and checked them while iterating the events
             for event in Event.objects.order_by('date')[:100]:
-                if event.date < cutoff:
+                if event.date > cutoff:
                     continue
 
                 self.logger.debug('Cleaning up %r' % event)
                 event.delete()
 
             for group in Group.objects.order_by('last_seen')[:100]:
-                if group.date < cutoff:
+                if group.last_seen > cutoff:
                     continue
 
                 self.logger.debug('Cleaning up %r' % group)
