e59466d73a7ea5528ca5f0c79ddfaeb6fa744719
David Cramer <dcramer@gmail.com>
2011-06-25 12:49:38 +0200

    Integration test for Http interface

--- tests/test_interfaces/test_http.py
+++ tests/test_interfaces/test_http.py
@@ -1,7 +1,33 @@
 from .. import BaseTest
 
+from sentry import capture
+from sentry.models import Event
 from sentry.core.interfaces import Http
 
+class HttpIntegrationTest(BaseTest):
+    def test_create(self):
+        event_id = capture('Message', message='hello world', data={
+            'sentry.core.interfaces.Http': {
+                'url': 'http://example.com/foo/?bar=baz',
+                'method': 'GET',
+            }
+        })
+
+        event = Event.objects.get(event_id)
+
+        self.assertTrue('sentry.core.interfaces.Http' in event.data)
+        
+        result = event.data['sentry.core.interfaces.Http']
+
+        self.assertTrue('url' in result, result)
+        self.assertEquals(result['url'], 'http://example.com/foo/')
+        self.assertTrue('method' in result, result)
+        self.assertEquals(result['method'], 'GET')
+        self.assertTrue('data' in result, result)
+        self.assertEquals(result['data'], {})
+        self.assertTrue('querystring' in result, result)
+        self.assertEquals(result['querystring'], 'bar=baz')
+        
 class HttpTest(BaseTest):
     def test_serialize(self):
         http = Http('http://example.com/foo/', 'GET', {}, 'bar=baz')
