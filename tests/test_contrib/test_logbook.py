c5aaec2a544841c1855500c7b7c506f7ae24c10a
David Cramer <dcramer@gmail.com>
2011-06-25 22:22:11 +0800

    Added exception tests for logbook and logging modules

--- tests/test_contrib/test_logbook.py
+++ tests/test_contrib/test_logbook.py
@@ -25,3 +25,27 @@ class LogbookTest(BaseTest):
         tags = dict(event.tags)
         self.assertTrue('level' in tags)
         self.assertEquals(tags['level'], 'warning')
+
+    def test_exception(self):
+        logger = Logger('sentry.tests.test_contrib.test_logbook')
+        handler = SentryHandler('INFO')
+        with handler.applicationbound():
+            try:
+                raise ValueError('foo')
+            except:
+                logger.exception('foo bar')
+        
+        event = Event.objects.all()[0]
+
+        self.assertEquals(event.type, 'sentry.events.Exception')
+        self.assertEquals(event.time_spent, 0)
+        self.assertTrue('sentry.interfaces.Exception' in event.data)
+        event_data = event.data['sentry.interfaces.Exception']
+        self.assertTrue('type' in event_data)
+        self.assertEquals(event_data['type'], 'ValueError')
+        self.assertTrue('value' in event_data)
+        self.assertEquals(event_data['value'], 'foo')
+        
+        tags = dict(event.tags)
+        self.assertTrue('level' in tags)
+        self.assertEquals(tags['level'], 'error')
